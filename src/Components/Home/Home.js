9505db0336a1fd19fb00ef9b28533de09bfbf816
Akhtaruzzaman <akhtar01947@gmail.com>
2021-06-09 17:35:16 +0600

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Feature from '../Features/Features';
+import Hero from '../Hero/Hero';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero/>
+            <Feature/>
+        </div>
+    );
+};
+
+export default Home;
